6de157a58c9800099b4de0c02c60f1e0d7822c3a
Sanjievkumar <21cs101@kcgcollege.com>
2026-05-27 21:52:15 +0530

    fix: restore original CSS and main config from first commit

--- src/main.jsx
+++ src/main.jsx
@@ -1,14 +1,14 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import { BrowserRouter } from 'react-router-dom'
-import './styles/tokens.css'
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import { I18nextProvider } from 'react-i18next'
+import i18n from './i18n'
 import './index.css'
-import App from './App'
+import App from './App.jsx'
 
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <BrowserRouter>
+createRoot(document.getElementById('root')).render(
+  <StrictMode>
+    <I18nextProvider i18n={i18n}>
       <App />
-    </BrowserRouter>
-  </React.StrictMode>
+    </I18nextProvider>
+  </StrictMode>,
 )
